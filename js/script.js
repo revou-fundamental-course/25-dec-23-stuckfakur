ca94a219e1074b4670eea548a0d3c274e940538d
stuckfakur <benaleobayu@gmail.com>
2024-01-01 12:13:07 +0700

    update

--- js/script.js
+++ js/script.js
@@ -3,9 +3,23 @@ const st = document.getElementById('t');
 const s1 = document.getElementById('s1');
 const s2 = document.getElementById('s2');
 const s3 = document.getElementById('s3');
-document.getElementById('lformula').innerHTML = "L = 1/2 x a x t";
-document.getElementById('kformula').innerHTML = "K = a + b + c";
 
+function lForm(){
+    document.getElementById('lForm').style.display = "block";
+    document.getElementById('kForm').style.display = "none";
+    document.getElementById('choose').innerHTML = "Calculating Luas";
+    document.getElementById('choose').style.display = "block";
+    document.getElementById('lformula').innerHTML = "L = 1/2 x a x t";
+
+}
+function kForm(){
+    document.getElementById('lForm').style.display = "none";
+    document.getElementById('kForm').style.display = "block";
+    document.getElementById('choose').innerHTML = "Calculating Keliling";
+    document.getElementById('choose').style.display = "block";
+    document.getElementById('kformula').innerHTML = "K = a + b + c";
+
+}
 
 function lView(){
     document.getElementById('lSummary').style.display = "block";
@@ -30,8 +44,10 @@ function kView(){
     document.getElementById('kvaluesub').innerHTML = "K = " + kel1 + " + " + kel2 + " + " + kel3;
     document.getElementById('kvalue').innerHTML = "K = " + kelvalue;
 }
-function resetValue(){
+function lReset(){
     document.getElementById('lSummary').style.display = "none";
+}
+function kReset(){
     document.getElementById('kSummary').style.display = "none";
 }
 
